b9396322b3c778a3320b6dc5fa90fe9cefb7a751
Kuil <987083065@qq.com>
2016-03-28 23:22:22 +0800

    Update kuil.py

--- kuil.py
+++ kuil.py
@@ -1,19 +1,29 @@
 import subprocess
-a=open("./gtools.txt")
-f=a.readlines()
-a.close()
-title=[]
-links=[]
-for i in range(len(f)):
-    f[i]=f[i].split()
-for i in range(len(f)):
-    title.append(f[i][0])
-    links.append(f[i][1])
-print title,links
-for i in range(len(title)):
-    print title[i]
-    shells=subprocess.call("git clone "+links[i],shell=True)
-    if shells==0:
-        print title[i]+" Success\n"
-    else:continue
-    print "*********************************************"
+def ReadFile():
+    a=open("f:/py/gtools.txt")
+    f=a.readlines()
+    a.close()
+    Split(f)
+    
+def Split(f):
+    title=[]
+    links=[]
+    for i in range(len(f)):
+        f[i]=f[i].split()
+    for i in range(len(f)):
+        if f[i][0][0]=='#':continue
+        else:
+            title.append(f[i][0])
+            links.append(f[i][1])
+    print title,links
+
+def Down(title,links):
+    for i in range(len(title)):
+        print title[i]
+        shells=subprocess.call("git clone "+links[i],shell=True)
+        if shells==0:
+            print title[i]+"Success\n"
+        else:continue
+        print "*********************************************"
+if __name__ == '__main__':
+    ReadFile()
